00baea0bb58db8c3f15da6cc493f122638ac5854
ilker <ilkerciliv@gmail.com>
2022-07-05 12:24:31 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1> Hello World!</h1>
+    <h2>Hey</h2>
     <App />
   </React.StrictMode>
 );

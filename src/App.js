6e7d50d0683d21879f473aaadce4781500cfad2f
ilker <ilkerciliv@gmail.com>
2022-07-05 16:09:05 -0400

    added print console.log functionality to the search box, fixed method syntax

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import { robots } from "./robots";
 
 class App extends Component {
   constructor() {
-    super() // calls the constructor of the component, needed to use this function. 
+    super(); // calls the constructor of the component, needed to use this function.
     this.state = {
       robots: robots,
       searchfield: "",
     };
+  }
+  // Since app owns state, it can change its variables.
+
+  onSearchChange = (event) => { // When creating your own method on a state use following syntax to make sure that the [this] value is according to where its created.
+    console.log(event.target.value);
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
   }
 
   render() {
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots} />; 
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />;
       </div>
     );
   }
